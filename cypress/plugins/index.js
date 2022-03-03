fba3f45383bcd4c116176eca6a9a937414d1bf8b
phatle1991 <100836198+phatle1991@users.noreply.github.com>
2022-03-03 16:31:17 +0700

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,7 +2,7 @@
 // This example plugins/index.js can be used to load plugins
 //
 // You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
+// the plugins file with the 'pluginsFile' configuration optio.
 //
 // You can read more here:
 // https://on.cypress.io/plugins-guide
